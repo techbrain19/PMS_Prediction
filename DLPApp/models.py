7afc208120f83ca85a53435bc9d95d4a41b674d0
Techbrain <Techbrain19@gmail.com>
2024-06-10 16:12:37 +0100

    parting with coding among us

--- DLPApp/models.py
+++ DLPApp/models.py
@@ -1,32 +1,56 @@
-import os
-from sklearn.preprocessing import StandardScaler
-from keras.models import load_model
+import numpy as np
+import pandas as pd
 from django.db import models
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.svm import SVR
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.neural_network import MLPRegressor
+import xgboost as xgb
 
 class FuelPrediction(models.Model):
     date = models.DateField()
-    price = models.FloatField()
-    fuel_price_in_naira = models.FloatField(null=True, blank=True)
-    predicted_price = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    model_path = os.path.join(os.path.dirname(__file__), 'fuel_price_prediction_model.h5')
-    try:
-        model = load_model(model_path)
-    except OSError:
-        print("Error: Fuel price prediction model not found.")
-        model = None
-
-    scaler = StandardScaler()
+    fuel_price_in_naira = models.FloatField()
+    price = models.FloatField(default=0.0)
+    predicted_price = models.FloatField(default=0.0)
 
     def predict_fuel_price(self):
-        X = np.array([[self.date.year, self.date.month, self.date.day]])
-        X = self.scaler.fit_transform(X)
+        try:
+            pms_price = pd.read_csv('data/crude_oil_price.csv')
+            geopolitacal_event = pd.read_csv('data/Pol_Data.csv')
+            exchange_rate = pd.read_csv('data/USD_NGN_Historical_Data.csv')
+            data = pd.concat([pms_price, geopolitacal_event, exchange_rate], ignore_index=True)
+            data['date'] = pd.to_datetime(data['date'])
+            X = data['date'].values.reshape(-1, 1)
+            y = data['price']
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            models = {
+                'Linear Regression': LinearRegression(),
+                'SVR': SVR(),
+                'GBR': GradientBoostingRegressor(),
+                'MLP': MLPRegressor(),
+                'XGBoost': xgb.XGBRegressor()
+            }
+            best_r2 = -float('inf')
+            best_model = None
+            for name, model in models.items():
+                model.fit(X_train, y_train)
+                y_pred = model.predict(X_test)
+                mse = mean_squared_error(y_test, y_pred)
+                mae = mean_absolute_error(y_test, y_pred)
+                r2 = r2_score(y_test, y_pred)
+                print(f"{name} - MSE: {mse:.2f}, MAE: {mae:.2f}, R-squared: {r2:.2f}")
+                if r2 > best_r2:
+                    best_model = model
+                    best_r2 = r2
 
-        if self.model is not None:
-            self.predicted_price = self.model.predict(X)[0][0]
-        else:
-            self.predicted_price = 0.0
-
-        return self.predicted_price
+            date_input = np.array([[self.date.to_numpy()]]).reshape(-1, 1)
+            if self.date <= data['date'].max():
+                predicted_price = data[data['date'] == self.date]['price'].values[0]
+            else:
+                predicted_price = best_model.predict(date_input)[0]
+            return predicted_price
+        except (FileNotFoundError, ValueError, IndexError) as e:
+            print(f"Error occurred: {e}")
+            return None
--- DLPApp/views.py
+++ DLPApp/views.py
@@ -7,18 +7,22 @@ def index(request):
         form = FuelPredictionForm(request.POST)
         if form.is_valid():
             date = form.cleaned_data['date']
-            fuel_price_in_naira = form.cleaned_data['price']
-            fuel_prediction = FuelPrediction(date=date, fuel_price_in_naira=fuel_price_in_naira)
-            predicted_price = fuel_prediction.predict_fuel_price()
-            fuel_prediction.price = 0.0
-            fuel_prediction.predicted_price = predicted_price
-            fuel_prediction.save()
-
-            return redirect('result', pk=fuel_prediction.id)
+            fuel_prediction = FuelPrediction(date=date)
+            try:
+                predicted_price = fuel_prediction.predict_fuel_price()
+                if predicted_price is not None:
+                    fuel_prediction.predicted_price = predicted_price
+                    fuel_prediction.save()
+                    return redirect('result', pk=fuel_prediction.id)
+                else:
+                    error_message = "Error occurred while predicting fuel price."
+                    return render(request, 'DLPApp/index.html', {'form': form, 'error_message': error_message})
+            except (FileNotFoundError, ValueError, IndexError) as e:
+                error_message = f"Error occurred: {e}"
+                return render(request, 'DLPApp/index.html', {'form': form, 'error_message': error_message})
     else:
         form = FuelPredictionForm()
-    return render(request, 'DLPApp/index.html',  {'form': form})
-
+    return render(request, 'DLPApp/index.html', {'form': form})
 
 def result(request, pk):
     fuel_prediction = FuelPrediction.objects.get(pk=pk)
